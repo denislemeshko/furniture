1dd6cd2d3e78ae39f6075c60751e08d7fc2b8dc0
DenisLemeshko <lemeshko.denis@yandex.ru>
2021-05-18 10:49:52 +0300

    blog start

--- app/js/main.js
+++ app/js/main.js
@@ -7,12 +7,23 @@ $(function () {
 		$('.rightside-menu').addClass('rightside-menu--close');
 	});
 
+//первый слайдер
+
 	$('.top__slider').slick({
 		dots: true,
 		arrows: false,
 		fade: true,
 		autoplay: true
 	});
+// второй слайдер
+
+	$('.contact-slider').slick({
+		slidesToShow: 10,
+		slidesToScroll: 10,
+		dots: true,
+		arrows: false
+	});
+//
 
 	let mixer = mixitup('.gallery__inner');
 
