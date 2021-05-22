cfe447384994a19397500c40b252c0c52c375b98
DenisLemeshko <lemeshko.denis@yandex.ru>
2021-05-23 00:35:51 +0300

    adoptive start

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,15 @@ $(function () {
 		$('.rightside-menu').addClass('rightside-menu--close');
 	});
 
+
+
+	//меню при адоптации в хедере
+
+	$('.header__btn-menu').on('click', function() {
+		$('.menu').toggleClass('menu--open');
+	});
+
+
 //первый слайдер
 
 	$('.top__slider').slick({
@@ -27,7 +36,7 @@ $(function () {
 	$('.article-slider__box').slick({
 		prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="img"></button>',
 		nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="img"></button>',
-		
+
 	});
 
 	let mixer = mixitup('.gallery__inner');
